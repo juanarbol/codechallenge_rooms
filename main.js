3d99945ef6183699619566df5c87d62399537f54
Juan José <soyjuanarbol@gmail.com>
2018-01-12 15:13:12 -0500

    Main file for runing example proposed in code challenge

---
+++ main.js
@@ -0,0 +1,7 @@
+// This file is creted just for run the same example of code challenge
+const countLowerOrEqual = require('./countLowerOrEqual')
+
+const maxes = [1, 3, 6, 9, 35]
+const numbers = [6, 7, 8, 9, 3, 4, 6, 1, 1, 1, 2, 3, 10, 37, 3]
+
+console.log(countLowerOrEqual(maxes, numbers))
